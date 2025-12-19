aae02e29591a52919538d4ed35cd3d8e1753439d
Nongtaeeiei <134826394+potae112@users.noreply.github.com>
2025-12-19 16:23:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function createRoom() {
+  const room = Math.floor(1000 + Math.random() * 9000);
+  window.location.href = `room.html?room=${room}&owner=1`;
+}
 
+function joinRoom() {
+  const room = document.getElementById("roomInput").value;
+  if (!room) return alert("ใส่เลขห้อง");
+  window.location.href = `room.html?room=${room}`;
+}
