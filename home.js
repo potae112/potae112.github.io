c734fd7fc33cfca18d740708075815bf389102d0
Nongtaeeiei <134826394+potae112@users.noreply.github.com>
2025-12-19 16:16:22 +0700

    Update home.js

--- home.js
+++ home.js
@@ -1,11 +1,1 @@
-function createRoom(){
-  const room = Math.floor(1000 + Math.random()*9000);
-  localStorage.setItem("roomOwner", room);
-  location.href = `room.html?room=${room}&owner=1`;
-}
 
-function joinRoom(){
-  const room = document.getElementById("roomInput").value;
-  if(!room) return alert("ใส่เลขห้องก่อน");
-  location.href = `room.html?room=${room}`;
-}
